9ababb47ee8df2044369780b69f12bdabd33b8a4
Néstor Sabater <web.nsabater@gmail.com>
2018-10-18 19:46:22 +0200

    map UI finished

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
-var AvailableRegions = ["BE","FR","BG","DK","HR","DE","BA","HU","JE","FI","BY","GR","NL","PT","LI","LV","LT","LU","FO","PL","XK","CH","EE","IS","AL","IT","GG","CZ","IM","GB","AX","IE","ES","ME","MD","RO","RS","MK","SK","MT","SI","SM","UA","AT"];
-var UnavailableRegions = ["RU","SE","NO","AD"]; // To disable them from selection and color
+var AvailableRegions = ["BE","FR","BG","DK","HR","DE","BA","HU","JE","BY","GR","NL","PT","LI","LV","LT","LU","FO","PL","XK","CH","EE","IS","AL","IT","GG","CZ","GB","AX","IE","ES","ME","MD","RO","RS","MK","SK","MT","SI","SM","UA","AT"];
+var UnavailableRegions = ["RU","SE","NO","AD","FI","IM"]; // To disable them from selection and color
+var AutozoomRegions = ["VA","MC","SM","LI","MT","AD","LU","CY","XK","ME","GR","AL","MK"];
 var PlayerColors = ["#FF0C0C","#0C7AFF","#AAFF0C", "#e02aa9"]; // max number of players (4)
 var NumPlayers = 3; // Number of players
 
@@ -30,8 +31,10 @@ function InitPlayers(){
             Scores[i].push(AvailableRegions.pop());
     }
 
-    if(!Number.isInteger(RegionsPerPlayer)) // If not exact regions, give them randomly to last player to play
-        Scores[Scores.length-1].push(AvailableRegions.pop())
+    if(!Number.isInteger(RegionsPerPlayer)) { // If not exact regions, give them randomly to last player to play
+        Scores[Scores.length - 2].push(AvailableRegions.pop())
+        Scores[Scores.length - 1].push(AvailableRegions.pop())
+    }
 
     console.log(["Player regions (Scores):", Scores])
 }
@@ -59,8 +62,30 @@ function IsRegionEnabled(code){
 }
 
 
-function ChallengeRegion(){
+function ChallengeRegion(Challenger, Challenged){
 
+}
+
+function AutoZoom(map, code){
+    if(AutozoomRegions.indexOf(code) !== -1)
+        FocusOnRegion(map, code);
+}
+function FocusOnRegion(map, code){
+    map.setFocus({region: code, animate:true})
+}
+function UnFocus(map){
+    map.setFocus({regions: ["PT","DK","EE","UA"], animate:true})
+}
+
+function PassTurn(){
+    Turn++;
+    if(Turn > NumPlayers-1)
+        Turn = 0;
+    return Turn;
+}
+
+function CanSelect(code){
+    return Scores[Turn].indexOf(code) != -1
 }
 
 
@@ -71,15 +96,19 @@ function SelectRegion(event, code, isSelected, region){
     //if(!IsRegionEnabled(code)) // not needed here but onClick
      //   event.preventDefault()
 
-    if(region.length == 0)
+    /*if(region.length == 0) {
         SelectedRegion = null;
-    else
-        SelectedRegion = code;
+    }else {
+        if (SelectedRegion != null)
+            ChallengeRegion(SelectedRegion, code)
+        else
+            SelectedRegion = code;
+    }*/
     console.log(event)
     console.log(code)
     console.log(isSelected)
     console.log(region)
-    //ChallengeRegion()
+
 }
 
 
@@ -108,4 +137,39 @@ function ShuffleArray(array) {
     }
 
     return array;
+}
+
+
+/**
+ AJAX
+ **/
+function GetVecindades(pais){
+    const endpointUrl = 'https://query.wikidata.org/sparql',
+        sparqlQuery = `
+    #fronteras de hungria Q28
+    SELECT ?codLabel WHERE {
+      ?pais wdt:P31 wd:Q6256.
+      ?pais wdt:P17 wd:${pais}.
+      ?pais wdt:P36 ?capital.
+      ?pais wdt:P47 ?vecinos.
+      ?vecinos wdt:P1813 ?cod
+      SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+      
+    }`,
+        fullUrl = endpointUrl + '?query=' + encodeURIComponent( sparqlQuery ),
+        headers = { 'Accept': 'application/sparql-results+json' };
+
+    var vecinos = [];
+    fetch( fullUrl, { headers } ).then( body => body.json() ).then( json => {
+        const { head: { vars }, results } = json;
+        for ( const result of results.bindings ) {
+            for ( const variable of vars ) {
+                vecinos.push(result[variable].value);
+            }
+
+        }
+    } );
+
+    return vecinos;
+
 }
